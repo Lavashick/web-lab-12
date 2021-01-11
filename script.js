19b66763729517c98071c743a0473e9cb1b23016
Lavashick <lavashick@icloud.com>
2021-01-11 19:15:53 +0300

    готово

--- script.js
+++ script.js
@@ -5,6 +5,45 @@ var app = new Vue({
     placeholderName: "Введите имя",
     inputValueSurname: "",
     inputValueName: "",
-  }
-  
+    canSend: false,
+    isDisabled: true,
+    notes: [],
+  },
+
+  methods: {
+    addNewNote() {
+      if (this.inputValueName !== "" && this.inputValueSurname !== "") {
+        this.notes.push(
+          `${cFirstLet(this.inputValueSurname)} ${cFirstLet(
+            this.inputValueName
+          )}`
+        );
+        this.inputValueName = "";
+        this.inputValueSurname = "";
+      }
+    },
+  },
+
+  watch: {
+    inputValueName(value) {
+      if (value.length > 0 && this.inputValueSurname.length > 0) {
+        this.isDisabled = false;
+      } else {
+        this.isDisabled = true;
+      }
+    },
+
+    inputValueSurname(value) {
+      if (value.length > 0 && this.inputValueName.length > 0) {
+        this.isDisabled = false;
+      } else {
+        this.isDisabled = true;
+      }
+    },
+  },  
 });
+
+function cFirstLet(word) {
+  return word[0].toUpperCase() + word.toLowerCase().slice(1);
+}
+
