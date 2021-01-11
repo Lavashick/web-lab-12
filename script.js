171e677c0ee0c7619edb7cd7f67b91493f369438
Lavashick <lavashick@icloud.com>
2021-01-11 18:55:05 +0300

    Фронт

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-$(document).ready(function() {
-
-})
+var app = new Vue({
+  el: '#app',
+  data: {
+    placeholderSurname: "Введите фамилию",
+    placeholderName: "Введите имя",
+    inputValueSurname: "",
+    inputValueName: "",
+  }
+  
+});
